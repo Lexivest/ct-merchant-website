7e987afc92c992fa54c73c2d6361f472efc1333a
Lexivest <kadazon2@gmail.com>
2026-01-25 22:33:37 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./*.html", "./src/**/*.{js,html}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Plus Jakarta Sans"', 'sans-serif'],
+      },
+      colors: {
+        brand: {
+          purple: '#2E1065',
+          purpleLight: '#5B21B6',
+          pink: '#DB2777',
+          dark: '#0F172A',
+          light: '#F8FAFC',
+        },
+      },
+    },
   },
   plugins: [],
 }
