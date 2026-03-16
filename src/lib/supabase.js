3fc4a7b1ca56c4e8f76606363937aeefc522e1ed
Alexo <kadazon2@gmail.com>
2026-03-16 22:10:45 +0100

    Update website

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js"
 
 const SUPABASE_URL = "https://xdchacdjcgazyckacbpc.supabase.co"
 const SUPABASE_ANON_KEY =
-  "YOUR_SUPABASE_ANON_KEY_HERE"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkY2hhY2RqY2dhenlja2FjYnBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1NDA2MzMsImV4cCI6MjA4NTExNjYzM30.41V3RaUX-ii-EHysbcVpUCgm0-RsNmuOb8FmYsz72Ow"
 
 if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
   throw new Error("Supabase environment variables are missing.")
