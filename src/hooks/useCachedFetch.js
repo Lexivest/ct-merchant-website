a1e543fcb251c47051eb7a8379571b3cd1dbe9f8
Alexo <kadazon2@gmail.com>
2026-04-12 21:36:47 +0100

    Update website

--- src/hooks/useCachedFetch.js
+++ src/hooks/useCachedFetch.js
@@ -246,6 +246,7 @@ export default function useCachedFetch(queryKey, fetchPromise, options = {}) {
               removeSessionCacheEntry(queryKey)
             }
           }
+        console.error(`[DEBUG] useCachedFetch Caught Error on ${queryKey}:`, err)
           setTick(t => t + 1)
         }
       }
