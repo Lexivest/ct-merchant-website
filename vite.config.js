3c3bae4dea9eb66dee6156f1bd07622d7cbbc827
Alexo <kadazon2@gmail.com>
2026-04-13 14:00:41 +0100

    Update website

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Disable module preloading to test Firefox compatibility
+    modulePreload: false,
+  }
 })
