b52f20bbc1fd3cf022554211caf0a5fa3a60b393
Alexo <kadazon2@gmail.com>
2026-04-18 04:08:53 +0100

    Update website

--- src/pages/vendors/MerchantVideoKYC.jsx
+++ src/pages/vendors/MerchantVideoKYC.jsx
@@ -12,7 +12,7 @@ import {
 import { supabase } from "../../lib/supabase";
 import useAuthSession from "../../hooks/useAuthSession";
 import usePreventPullToRefresh from "../../hooks/usePreventPullToRefresh";
-import { invalidateCachedFetchStore } from "../../hooks/useCachedFetch";
+import { clearCachedFetchStore } from "../../hooks/useCachedFetch";
 import { useGlobalFeedback } from "../../components/common/GlobalFeedbackProvider";
 import GlobalErrorScreen from "../../components/common/GlobalErrorScreen";
 import { getFriendlyErrorMessage } from "../../lib/friendlyErrors";
